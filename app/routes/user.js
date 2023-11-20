20cdc614fa7bc25daf1f1828131d08729bfe4af9
kirilll.212 <mac@Kirils-MacBook-Pro-15.local>
2023-11-20 13:57:27 +0200

    s

--- app/routes/user.js
+++ app/routes/user.js
@@ -3,7 +3,7 @@ const userController = require('../controllers/userController')
 module.exports = (router) => {
     router.post('/register', userController.register)
     router.post('/login', userController.login)
-    router.get('/profile/:id', userController.login)
+    router.get('/profile/:id', userController.getUserById)
     router.post('/forgot-password', userController.forgotPassword)
     router.post('/reset-password', userController.resetPassword)
     router.post('/send-request', userController.sendRequest)
